ca23c006b96d390fb53b3bbb8d7227d5d603d86f
Dheeraj Gupta <dheeraj200011@gmail.com>
2025-07-29 09:16:08 +0530

    get profile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -19,3 +19,30 @@ export const userSignup = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const user = await User.find();
+    return res
+      .status(200)
+      .json({ message: "Users fetched successfully", user });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getUserByName = async (req, res) => {
+  const { firstName } = req.body;
+  try {
+    const user = await User.findOne({ firstName });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    } else {
+      return res.status(200).json({ message: "User found", user });
+    }
+  } catch (error) {
+    console.error("Error fetching user by name:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { userSignup } from "../controllers/user.controller.js";
+import {
+  getAllUsers,
+  getUserByName,
+  userSignup,
+} from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.post("/signup", userSignup);
+router.get("/feed", getAllUsers); // feed api: get all users
+router.get("/getUserByName", getUserByName); // get user by name
 
 export default router;
