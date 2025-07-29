5f618c420a944de49efd3a0f2c6fde07ce812264
Dheeraj Gupta <dheeraj200011@gmail.com>
2025-07-29 10:01:34 +0530

    MORE ROUTES

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -46,3 +46,43 @@ export const getUserByName = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  const id = req.body.id;
+  console.log("Deleting user with ID:", id);
+  try {
+    const user = await User.findByIdAndDelete({ _id: id });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { id, firstName, lastName, email } = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        firstName,
+        lastName,
+        email,
+      },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({
+      message: "User updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,17 @@
 import express from "express";
 import {
+  deleteUser,
   getAllUsers,
   getUserByName,
   userSignup,
+  updateUser,
 } from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.post("/signup", userSignup);
 router.get("/feed", getAllUsers); // feed api: get all users
 router.get("/getUserByName", getUserByName); // get user by name
+router.get("/delete", deleteUser); // delete user by name
+router.patch("/updateuser", updateUser); // update user by name
 
 export default router;
