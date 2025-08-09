fe4965980b5aff8790b91e43391c9cc34be3bdf8
Dheeraj Gupta <dheeraj200011@gmail.com>
2025-08-09 12:10:02 +0530

    dne

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,17 +1,19 @@
-export const loginUrl = "http://localhost:3000/api/v1/auth/login";
-export const signupUrl = "http://localhost:3000/api/v1/auth/signup";
-export const logoutUrl = "http://localhost:3000/api/v1/auth/logout";
-export const getprofile = "http://localhost:3000/api/v1/profile/getuserprofile";
-export const getFeeds = "http://localhost:3000/api/v1/user/feed";
+export const loginUrl = "http://http://54.242.172.199/api/api/v1/auth/login";
+export const signupUrl = "http://54.242.172.199/api/api/v1/auth/signup";
+export const logoutUrl = "http://54.242.172.199/api/api/v1/auth/logout";
+export const getprofile =
+  "http://54.242.172.199/api/api/v1/profile/getuserprofile";
+export const getFeeds = "http://54.242.172.199/api/api/v1/user/feed";
 export const editProfileUrl =
-  "http://localhost:3000/api/v1/profile/editprofile";
+  "http://54.242.172.199/api/api/v1/profile/editprofile";
 export const getAllConnectionUrl =
-  "http://localhost:3000/api/v1/user/connections";
+  "http://54.242.172.199/api/api/v1/user/connections";
 
-export const getAllRequsetUrl = "http://localhost:3000/api/v1/user/requests";
+export const getAllRequsetUrl =
+  "http://54.242.172.199/api/api/v1/user/requests";
 
 export const sendConnectionRequest =
-  "http://localhost:3000/api/v1/request/send";
+  "http://54.242.172.199/api/api/v1/request/send";
 
 export const reviewConnectionRequest =
-  "http://localhost:3000/api/v1/request/review/:status/:requestId";
+  "http://54.242.172.199/api/api/v1/request/review/:status/:requestId";
