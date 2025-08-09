a4b2067f6f89eb87957279d2308bfeae7b862d3c
Dheeraj Gupta <dheeraj200011@gmail.com>
2025-08-09 12:32:57 +0530

    comp

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,14 +1,12 @@
-export const loginUrl = "/api/api/v1/auth/login";
-export const signupUrl = "/api/api/v1/auth/signup";
-export const logoutUrl = "/api/api/v1/auth/logout";
-export const getprofile = "/api/api/v1/profile/getuserprofile";
-export const getFeeds = "/api/api/v1/user/feed";
-export const editProfileUrl = "/api/api/v1/profile/editprofile";
-export const getAllConnectionUrl = "/api/api/v1/user/connections";
+export const BASE_URL = "http://54.242.172.199/api";
 
-export const getAllRequsetUrl = "/api/api/v1/user/requests";
-
-export const sendConnectionRequest = "/api/api/v1/request/send";
-
-export const reviewConnectionRequest =
-  "/api/api/v1/request/review/:status/:requestId";
+export const loginUrl = `${BASE_URL}/v1/auth/login`;
+export const signupUrl = `${BASE_URL}/v1/auth/signup`;
+export const logoutUrl = `${BASE_URL}/v1/auth/logout`;
+export const getprofile = `${BASE_URL}/v1/profile/getuserprofile`;
+export const getFeeds = `${BASE_URL}/v1/user/feed`;
+export const editProfileUrl = `${BASE_URL}/v1/profile/editprofile`;
+export const getAllConnectionUrl = `${BASE_URL}/v1/user/connections`;
+export const getAllRequsetUrl = `${BASE_URL}/v1/user/requests`;
+export const sendConnectionRequest = `${BASE_URL}/v1/request/send`;
+export const reviewConnectionRequest = `${BASE_URL}/v1/request/review/:status/:requestId`;
