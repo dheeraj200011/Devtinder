df2aaa1fd5488758949e218582e37fad5d9e58c1
Dheeraj Gupta <dheeraj200011@gmail.com>
2025-08-11 11:04:27 +0530

    done

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI ||
-        "mongodb+srv://dheeraj200011:iqoVuilNm9sXO1k1@cluster0.n3wmuoa.mongodb.net/devtinder"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
